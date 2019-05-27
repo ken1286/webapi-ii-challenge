722ba54e329a7498433753ea620a9541a4f6b2d2
ken1286 <kindle1286@gmail.com>
2019-05-27 14:38:50 -0700

    route set up with get requests working

---
+++ posts/posts-router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const db = require('../data/db.js');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  
+  db.find()
+    .then( posts => {
+      res.status(200).json({ posts });
+    })
+    .catch( err => {
+      res.status(500).json({ error: "The posts information could not be retrieved." });
+    })
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db
+    .findById(id)
+    .then( post => {
+      res.status(200).json({ post })
+    })
+    .catch( err => {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+})
+
+
+
+module.exports = router;
